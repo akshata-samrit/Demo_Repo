ffd134c1120f5db88c6b117b7582c4683d01dcad
akshata-samrit <akshatabadwaik@gmail.com>
2021-10-29 12:33:49 +0530

    My First Commit

---
+++ src/Demo_Package/TC001.java
@@ -0,0 +1,9 @@
+package Demo_Package;
+
+public class TC001 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("My First Test Case");
+	}
+}
---
+++ src/Demo_Package/TC002.java
@@ -0,0 +1,9 @@
+package Demo_Package;
+
+public class TC002
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("My Second Test Case");
+	}
+}
